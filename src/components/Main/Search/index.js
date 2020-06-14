da4accd74072e930b5a12527eac6198b998ced3d
Amanda Barboza <amanda.barboza@mirumagency.com>
2020-06-14 18:15:19 -0300

    alteracao de nome de componente

--- src/components/Main/Search/index.js
+++ src/components/Main/Search/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Title, Description } from "../../Global"
-import MovieList from "../../List"
+import MovieList from "../../SearchList"
 
 const Find = () => (
   <>
